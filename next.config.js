70dd0a9539f9c19fa28d9b79403eb128a232a772
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2024-05-10 23:45:44 -0400

    app

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com', 'another-domain.com'],
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
+  },
+}
+
+module.exports = nextConfig
